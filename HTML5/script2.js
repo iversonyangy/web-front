8ffd848c2d6a398ecf44d32f9d7020ca156bb5d0
iversonyangy <623477242@qq.com>
2017-04-13 19:42:34 +0800

    这会应该对了吧

---
+++ HTML5/script2.js
@@ -0,0 +1,12 @@
+function draw(id) {
+    var canvas = document.getElementById(id);
+    if (canvas == null)
+        return false;
+    var content = canvas.getContext('2d');
+    content.fillStyle = "#EEEEEF";
+    content.fillRect(0, 0, 400, 300);
+    content.fillStyle = "red";
+    content.strokeStyle = "blue";
+    content.lineWidth = 1;
+    content.fillRect(50,50,100,100);
+}
