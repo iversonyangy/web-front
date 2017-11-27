f7419d1c9a4986324d3f6af3f778f5e4574e3ce5
Iversonyangy <623477242@qq,com>
2017-11-27 21:39:29 +0800

    JS写比较函数

---
+++ js/比较函数.js
@@ -0,0 +1,13 @@
+function  compare(value1, value2)  {
+    if (value < value2) {
+        return -1;
+    } else if (value1 > value2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+var values = [0, 1, 5, 10, 15];
+values.sort(compare);
+alert(value);
